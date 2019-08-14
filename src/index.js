cf017fd55466b16d6310edca84b888f330d9cec1
Your Name <baylyd@email.arizona.edu>
2019-08-14 14:11:42 -0700

    adapt to single brain slice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-import data from "./gz_2010_us_040_00_500k.json"
-let globals =[1000,1000,-1000,-1000]
+import geodata from "./gz_2010_us_040_00_500k.json"
+import brain from "./GeoJson_Brains/ax_108_inf_-18mm.json"
+
+let interpolator = () => {
+  let ob = {}
+  ob.setup = (x0,y0,x1,y1) => {
+    ob.x0 = x0
+    ob.y0 = y0
+    ob.x1 = x1
+    ob.y1 = y1
+  }
+  ob.calc = (x) => {
+    return (ob.y1-ob.y0)/(ob.x1 -ob.x0)*x + (ob.x1*ob.y0 - ob.y1 * ob.x0)/(ob.x1 - ob.x0)
+  }
+  return ob
+}
+
+let globals =[33,65,320,391]
 let drawLine = (linedata,ctx)=> {
   let ob = {}
   // need canvas ref
+  //create interpolator
+  //map xmin - xmax to 0 to 5000 or whatever width is do the same for y
+  let xinterp = interpolator()
+  xinterp.setup(globals[0],0,globals[2],5000)
+  let yinterp = interpolator()
+  yinterp.setup(globals[1],5000,globals[3],0)
   ob.draw =() => {
     ctx.beginPath()
     let first = linedata[0]
-    ctx.moveTo(20*(first[0]+180),20*(first[1]))
+    let x = xinterp.calc(first[0])
+    let y = yinterp.calc(first[1])
+
+    ctx.moveTo(x,y)
     for (let i = 1; i < linedata.length;i++) {
       let pt = linedata[i]
-      ctx.lineTo(20*(pt[0]+180),20*(pt[1]) )
+      let x = xinterp.calc(pt[0])
+      let y = yinterp.calc(pt[1])
+      ctx.lineTo(x,y)
       // update the data
       if (pt[0] < globals[0]) {
         globals[0] = pt[0]
@@ -47,8 +74,9 @@ let dataBind = (drawing,data_to_bind) => {
   // drawing has can and ctx attributes
   // find a way get to the data we need here
   let iter = 0  
+  console.log("number of total coordinates ",data_to_bind.geometry.coordinates.length)
   for (line of data_to_bind.geometry.coordinates) {
-  let data_bound = line[0]
+  let data_bound = line
   let line = drawLine(data_bound,drawing.ctx)
   line.draw()
     iter +=1
@@ -58,6 +86,7 @@ let dataBind = (drawing,data_to_bind) => {
 let featurePass = (drawing,upperData) => {
   let ob = {}
   ob.mapFeatures = () => {
+    console.log("total features ",upperData.features.length)
   for (let feature of upperData.features) {
     let db = dataBind(drawing,feature)
   }
@@ -71,7 +100,9 @@ let featurePass = (drawing,upperData) => {
 // aim for as much functional as possible
 
 let drawing = setup()
+console.log(brain)
 drawing.begin(5000,5000)
-let allfeatures = featurePass(drawing,data)
+let allfeatures = featurePass(drawing,brain)
 allfeatures.mapFeatures()
+console.log(geodata)
 console.log(globals)
