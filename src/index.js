9eae23ecd2cd6812712f592790314d50112b9c33
Your Name <baylyd@email.arizona.edu>
2019-08-29 14:30:48 -0700

    add color based on data file, still hard coded

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import geodata from "./gz_2010_us_040_00_500k.json"
 import brain from "./GeoJson_Brains/sag_228_L_-24mm.json"
+import csvData from "./WeaveTutorial/Tables/indiv_run_summary_pos_thresh_ice_perc_sum.csv"
 
 let interpolator = () => {
   let ob = {}
@@ -16,7 +17,15 @@ let interpolator = () => {
 }
 
 let globals =[41,15,392,298]
+let LerpCol = (c1,c2,t) => {
+  let red = c1.r + ( c2.r - c1.r) *t
+  let blue = c1.b + ( c2.b - c1.b) *t
+  let green = c1.g + ( c2.g - c1.g) *t
+  return `rgb(${red},${green},${blue})`
+
+}
 let drawLine = (linedata,ctx)=> {
+  //linedata  = {points,region}
   let ob = {}
   // need canvas ref
   //create interpolator
@@ -27,13 +36,23 @@ let drawLine = (linedata,ctx)=> {
   yinterp.setup(globals[1],500,globals[3],0)
   ob.draw =() => {
     ctx.beginPath()
-    let first = linedata[0]
+    let red = {
+      r:255,
+      g:0,
+      b:0,
+    }
+    let yellow = {
+      r:255,
+      g:236,
+      b:0
+    }
+    let first = linedata.points[0]
     let x = xinterp.calc(first[0])
     let y = yinterp.calc(first[1])
 
     ctx.moveTo(x,y)
-    for (let i = 1; i < linedata.length;i++) {
-      let pt = linedata[i]
+    for (let i = 1; i < linedata.points.length;i++) {
+      let pt = linedata.points[i]
       let x = xinterp.calc(pt[0])
       let y = yinterp.calc(pt[1])
       ctx.lineTo(x,y)
@@ -53,8 +72,21 @@ let drawLine = (linedata,ctx)=> {
     }
     ctx.closePath()
     ctx.stroke()
-    ctx.fillStyle=`rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
-    ctx.fill()
+    for (let row of csvData) {
+      // the important parts of the dataset are in column 0 and column 9
+      if ( linedata.region == row[0] ) {
+        // the min appears to be almost 0 and the max should come in around 0.006
+        let scanData = parseFloat(row[9])
+        if (! isNaN(scanData)) {
+          let  t = scanData/0.006 
+          console.log(t)
+          let lerpc = LerpCol(yellow,red,t)
+          ctx.fillStyle=lerpc
+          ctx.fill()
+        }
+        break
+      }
+    }
   }
   return ob
 }
@@ -79,9 +111,15 @@ let dataBind = (drawing,data_to_bind) => {
   let iter = 0  
   console.log("number of total coordinates ",data_to_bind.geometry.coordinates.length)
   for (line of data_to_bind.geometry.coordinates) {
-  let data_bound = line
-  let line = drawLine(data_bound,drawing.ctx)
-  line.draw()
+    //let data_bound = {coords:line,properties
+    //make copy of line data and bind in the region name
+    let drawingData = {
+      points:line,
+      region:data_to_bind.properties.regionName
+    }
+    console.log(data_to_bind.properties)
+    let line = drawLine(drawingData,drawing.ctx)
+    line.draw()
     iter +=1
   }
 }
@@ -101,7 +139,6 @@ let featurePass = (drawing,upperData) => {
 // lifted from stack overflow
 
 let getPos = (can,e) => {
-  debugger
   let rect = can.getBoundingClientRect()
   let x = e.clientX - rect.left
   let y = e.clientY - rect.top
@@ -121,11 +158,11 @@ console.log(brain)
 drawing.begin(500,500)
 let allfeatures = featurePass(drawing,brain)
 allfeatures.mapFeatures()
-console.log(geodata)
 console.log(globals)
 console.log(Math.floor(Math.random()*255))
 let can = document.querySelector("canvas")
 can.addEventListener("click",(e)=> {
   getPos(can,e)
 })
-
+console.log(csvData)
+let accessdata = csvData
